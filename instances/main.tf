5468f77bcc1ed910375924ba70cb4e0537d13317
ryantakeuchi <ryantakeuchi@live.com>
2021-01-21 07:47:09 -0800

    added apt upgrade to main.tf

--- instances/main.tf
+++ instances/main.tf
@@ -163,6 +163,7 @@ resource "null_resource" "configure-cat-app" {
     inline = [
       "sudo add-apt-repository universe",
       "sudo apt -y update",
+      "sudo apt -y upgrade",
       "sudo apt -y install apache2",
       "sudo systemctl start apache2",
       "sudo chown -R ubuntu:ubuntu /var/www/html",
