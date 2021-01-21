67571da9563a8076af4adeb7df0c06173b5d3555
ryantakeuchi <ryantakeuchi@live.com>
2021-01-21 06:09:51 -0800

    updated main.tf

--- instances/main.tf
+++ instances/main.tf
@@ -128,7 +128,6 @@ resource "aws_instance" "hashicat" {
     TTL = var.ttl
   }
 }
-# -------- added owner and ttl tags to resource ---------
 
 # We're using a little trick here so we can run the provisioner without
 # destroying the VM. Do not do this in production.
